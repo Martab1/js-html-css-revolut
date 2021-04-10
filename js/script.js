ddfa78f9570afc2852e24d9d2eb7543ea111859d
Martab1 <martabernardo86@gmail.com>
2021-04-10 15:40:44 +0200

    dropdown menu js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 // console.log("jQuery", $);
 
 
+$(document).ready( function(){
+
+    // DROPDOWN MENU
+
+    // ref
+
+var dropDown = $(".dropdown");
+var dropdownMenu = dropDown.children(".dropdown-menu");
+var dropDownLinks = dropDown.children("a");
+
+
+// click (mostrare e nascondere il menu associato)
+dropDownLinks.click( function(){
+
+   // quando clicco su una a prendo il dropdown associato 
+   var activeMenu = $(this).next(".dropdown-menu");
+   
+   // reset (se un dropdownMenu non è quello cliccato in quel momento richiudilo )
+   dropdownMenu.not(activeMenu).hide();
+
+   // show/hide ad ogni click
+   activeMenu.toggle();
+
+});
+
+
+    // end doc ready
+});
+
+
+
+
+
